2947acd3de94d0f5c7b265b4e4d3fdfe1105c599
DionCstS <dionathacastro1@gmail.com>
2024-12-19 18:15:16 -0300

    Criando Views Reutilizaveis

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -14,12 +14,8 @@
     @endif
     
     <form action="{{ route('users.update', $user->id) }}" method="POST">
-        @csrf
         @method('put')
-        <input type="text" name="name" placeholder="Nome" value="{{ $user->name }}">
-        <input type="email" name="email" placeholder="E-mail" value="{{ $user->email }}">
-        <input type="password" name="password" placeholder="Senha">
-        <button type="submit">Edit</button>
+        @include('admin.users.partials.form')
     </form>
 
 @endsection
